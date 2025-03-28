069c6c3e2f0485428e8b8899d3e887ce0eb53927
Yann Querlioz <zazbone@pm.me>
2025-03-28 14:29:12 +0100

    [FEAT DOC TEST] compat array_namespace
    array_namespace is the best way to generic code
    for all library supporting array-api-standard.
    Now NuKi have is own version, which will be used
    on every numerical functions of the framework.

---
+++ src/nuki/compat.py
@@ -0,0 +1,107 @@
+"""
+compat
+=====
+
+Module Overview
+---------------
+The compat.py module provides a collection of utility functions
+that facilitate compatibility between various array libraries,
+including numpy, jax, and others (in future).
+The goal is to enable the writing of framework-independent code
+by abstracting away differences between these libraries.
+The module also includes special handling to ensure that Python scalar
+values do not interfere with operations that expect array-like inputs.
+
+Available Functions
+-------------------
+array_namespace(*xs, api_version=DEFAULT_ARRAY_API_VERSION, use_compat=None)
+    Get the appropriate array namespace for the input arrays.
+erf(z)
+    Compute the error function (erf) of the input.
+erfc(z)
+    Compute the complementary error function (erfc) of the input.
+"""
+
+from numbers import Number
+
+import array_api_compat as aa
+from array_api_compat import numpy
+
+__all__ = ["array_namespace"]
+
+DEFAULT_ARRAY_API_VERSION = "v2022.12"
+
+
+def is_not_python_number(x):
+    """Check if `x` is not a Python scalar number.
+
+    This function determines whether the input `x`
+    is not an instance of a Python scalar number,
+    such as an `int`, `float`, or `complex`.
+
+    Parameters
+    ----------
+    x: Any
+        The input value to check.
+
+    Returns
+    -------
+    bool:
+        True if `x` is not a Python scalar number, False otherwise.
+    """
+    return not isinstance(x, Number)
+
+
+def filter_numbers(it):
+    """Filter out Python scalar numbers from an iterable.
+
+    This function removes scalar numbers (e.g., `int`, `float`, `complex`)
+    from an iterable, ensuring only array-like objects remain.
+
+    Parameters
+    ----------
+    it: Iterable[Array | Number]
+        An iterable containing array-like objects and/or Python scalar numbers.
+
+    Returns
+    -------
+    Iterator[Array]:
+        An iterator over the array-like objects.
+    """
+    return filter(is_not_python_number, it)
+
+
+def array_namespace(*xs, api_version=DEFAULT_ARRAY_API_VERSION, use_compat=None):
+    """Get the appropriate array namespace for the input arrays.
+
+    This function acts as a wrapper for `array_api_compat.array_namespace`
+    to avoid issues with Python scalar values.
+    It filters out scalar values from the input before determining the
+    correct array namespace.
+
+    Parameters
+    ----------
+    *xs: Array
+        A sequence of arrays or array-like objects.
+    api_version: str, optional
+        The version of the Array API standard to use (default is "v2022.12").
+    use_compat: bool, optional
+        Whether to use compatibility shims (default is None).
+
+    Returns
+    -------
+    module:
+        The appropriate array namespace module (e.g., `numpy`, `jax`, etc.).
+
+    Notes
+    -----
+    If no array-like objects are found, the function returns the `numpy` namespace.
+    """
+    fxs = filter_numbers(xs)
+    try:
+        first = next(fxs)
+    except StopIteration:
+        return numpy
+    return aa.array_namespace(
+        first, *fxs, api_version=api_version, use_compat=use_compat
+    )
---
+++ tests/test_compat.py
@@ -0,0 +1,61 @@
+import importlib
+
+import array_api_compat
+import numpy as np
+import pytest
+from numpy.testing import assert_allclose
+
+from nuki import compat
+
+try:
+    import jax
+except ModuleNotFoundError:
+    jax = None
+MODULES_NAMES = (
+    "cupy",
+    "dask.array",
+    "jax",
+    "numpy",
+    "torch",
+)
+
+
+@pytest.fixture
+def array_modules():
+    modules = []
+    for module_name in MODULES_NAMES:
+        try:
+            modules.append(importlib.import_module(f"array_api_compat.{module_name}"))
+        except ModuleNotFoundError:
+            pass
+    return modules
+
+
+def test_privates_functions():
+    assert not compat.is_not_python_number(10)
+    assert not compat.is_not_python_number(3.14)
+    assert not compat.is_not_python_number(2 + 3j)
+    assert compat.is_not_python_number(np.array([1, 2, 3]))
+    assert compat.is_not_python_number("string")
+
+    iterable = [1, 2.0, 3 + 4j, np.array([4, 5, 6]), "Hello world !"]
+    results = list(compat.filter_numbers(iterable))
+    expected = [np.array([4, 5, 6]), "Hello world !"]
+    assert len(results) == len(expected)
+    assert_allclose(results[0], expected[0])
+    assert results[1] == expected[1]
+
+
+def test_array_namespace(array_modules):
+    for xp in array_modules:
+        scalar1 = 12.8
+        arr1 = xp.asarray([1, 2, 3])
+        arr2 = xp.asarray([4, 5, 6])
+        namespace = compat.array_namespace(scalar1, arr1, arr2)
+        assert namespace is xp
+
+    scalar1 = 12.8
+    scalar2 = 10
+    scalar3 = 1.0 + 7j
+    namespace = compat.array_namespace(scalar1, scalar2, scalar3)
+    assert namespace is array_api_compat.numpy
