1953b4521bd0543fcea171028bccd597eeb68f04
Yann Querlioz <zazbone@pm.me>
2025-05-21 19:39:51 +0200

    [FIX] Attribute error Submodules are not present in parent __all__ field, resulting to an attribute errors when an acces throught module getattr was performed. With the relative import and the manual edition of __all__ field, the behavior is fixed.

--- src/nuki/__init__.py
+++ src/nuki/__init__.py
@@ -1,2 +1,8 @@
+from . import compat, distribution, histogram, special
+
+__all__ = ("compat", "distribution", "histogram", "special")
+__all__ = ("hello",)
+
+
 def hello() -> str:
     return "Hello from nuki!"
